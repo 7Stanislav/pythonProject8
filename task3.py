f68a1479bd1f815d2fd01a7c73470455e6be02ad
7Stanislav <112968442+7Stanislav@users.noreply.github.com>
2023-02-08 16:51:03 +0200

    Completed task 3

--- task3.py
+++ task3.py
@@ -8,3 +8,18 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+my_file = open('myfile2.txt', 'r', encoding='utf-8')
+content = my_file.readlines()
+
+count = 1
+while count <= len(content):
+    my_str = str(content[count - 1])
+    a = my_str.replace("One", "Один").replace("Two", "Два").replace\
+        ("Three", "Три").replace("Four", "Четыре")
+    my_new_file = open('mynewfile.txt', 'a', encoding='utf-8')
+    my_new_file.writelines(a)
+    my_new_file.close()
+    count += 1
+
+my_file.close()
