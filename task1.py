10c42b1c357b1f1649f87c726572a0fd4c5598c3
7Stanislav <112968442+7Stanislav@users.noreply.github.com>
2023-02-08 16:48:31 +0200

    Completed task 1

--- task1.py
+++ task1.py
@@ -3,3 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+my_file = open('myfile.txt', 'w', encoding='utf-8')
+line = input('Введите текст \n')
+
+while len(line) > 0:
+    my_file.writelines(f'{line}\n')
+    line = input('Введите текст \n')
+else:
+    my_file.close()
